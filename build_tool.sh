a390a12c63def893d5ec4fd983a06cee81dfa39e
MCVM <129513410+MinecraftVM@users.noreply.github.com>
2025-08-17 17:52:10 +0800

    Update build_tool.sh

--- build_tool.sh
+++ build_tool.sh
@@ -46,8 +46,8 @@ clean(){
 
 
 build_kernel(){
-	export KBUILD_BUILD_USER="zahi0"  
-	export KBUILD_BUILD_HOST="zahi0-server"  
+	export KBUILD_BUILD_USER="MinecraftVM"  
+	export KBUILD_BUILD_HOST="MinecraftVM-GitHub-Action"  
 	make $args wayne_defconfig #修改配置文件
 	if [ $? -ne 0 ]; then
       		exit 0
